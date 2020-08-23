b225884924d634270522aa1b946d26f0df1e8ab1
oofoofoof14 <49186603+oofoofoof14@users.noreply.github.com>
2020-08-23 16:46:24 +1000

    Create main.py

---
+++ main.py
@@ -0,0 +1,288 @@
+import requests, random, uuid, socket, ipaddress, webbrowser, platform, os, time
+
+#my_ip = ipaddress.ip_address('--0.--0.--0.-0') #Random choices and other shit goes here since it is just cleaner
+
+
+print('Tools pack')
+time.sleep(4)
+print(platform.system() + ',' + (platform.release()) + ' ' + 'Detected')
+time.sleep(1)
+print('Detecting ip.')
+        time.sleep(4)
+        print('000.000.000.0' + ' Has been deteced') #still working on it
+        time.sleep(1)
+        print('Checking OS...')
+        time.sleep(4)
+        if platform.system() == 'Linux':
+            print('Linux OS is not a valid os for the tool pack and is getting worked on!')
+            time.sleep(1)
+            a = int(input('Type any number for a surprise! '))
+            if a >= 1:
+                for _ in range(a):
+                    webbrowser.open(url='https://www.youtube.com/watch?v=oHg5SJYRHA0', new=1)
+                time.sleep(3)
+                print("TERMINATING NOW!")
+                time.sleep(5)
+                exit()
+
+
+
+
+usernamepass = input('Username:Password: ')
+time.sleep(4)
+print('Loading')
+if usernamepass == '': #this is getting re-done
+    print('Idk what im going to add here probbaly the tools ')
+else:
+    print('Incorrect, Executing Free version.')
+    time.sleep(10)
+
+    print('(##########)100% COMPLETE')
+    time.sleep(0.5)
+    print('Complete!')
+    print('Would you like to join the discord server? You can buy the full version there and also boosters will get more tools!')
+    discord = input('Y/N? ')
+    if (discord.capitalize()) == 'Y':
+        webbrowser.open(url='https://discord.gg/WWWhj9z')
+    while True:
+        time.sleep(2)
+        print('')
+        time.sleep(.1)
+        print('')
+        time.sleep(.1)
+        print('')
+        time.sleep(.1)
+        print('')
+        time.sleep(.1)
+        print('')
+        time.sleep(.1)
+        print('')
+        time.sleep(.1)
+        print('')
+        time.sleep(.1)
+        print('')
+        time.sleep(.1)
+        print('')
+        time.sleep(.1)
+        print('')
+        time.sleep(.1)
+        print('')
+        time.sleep(.1)
+        print('')
+        time.sleep(.1)
+        print('')
+        time.sleep(.1)
+        print('')
+        time.sleep(.1)
+        print('')
+        time.sleep(.1)
+        print('')
+        time.sleep(.1)
+        print('')
+        time.sleep(.1)
+        print('')
+        time.sleep(.1)
+        print('')
+        time.sleep(.1)
+        print('')
+        time.sleep(.1)
+        ab = ('search?q=nut&oq=nut&aqs=chrome..69i57.1106j0j7&sourceid=chrome&ie=UTF-8')
+        aba = ('search?q=stupid&oq=stupid&aqs=chrome..69i57j69i61.2619j0j9&sourceid=chrome&ie=UTF-8')
+        abb = ('search?q=idot&oq=idot&aqs=chrome..69i57.2133j0j9&sourceid=chrome&ie=UTF-8')
+        abc = ('search?q=history&oq=history&aqs=chrome..69i57j69i59j69i60.1730j0j7&sourceid=chrome&ie=UTF-8')
+        google_sites = random.choice([ab + aba + abb + abc]) #add more if you want to just use the end of a google search and remember '' or ""
+        random_sites = random.choice(['https://www.youtube.com/watch?v=i_2yNbIzwOI' +
+                                      'https://www.youtube.com/watch?v=dQw4w9WgXcQ' +
+                                      'https://www.youtube.com/watch?v=WXxV9g7lsFE' +
+                                      'https://downloadmoreram.com/download.html'
+                                      ]) # add more if u want to
+        print('  88888             8    888b.           8 ')
+        print("    8   .d8b. .d8b. 8    8  .8 .d88 .d8b 8.dP ")
+        print("    8   8' .8 8' .8 8    8wwP' 8  8 8    88b  ")
+        print("    8   `Y8P' `Y8P' 8    8     `Y88 `Y8P 8 Yb ")
+        time.sleep(2)
+        print('By sɹǝƃƃod#5183')
+        print('Credit to: zoony#1337 & Dropout#1337')
+        print('')
+        print('')
+        print('')
+        time.sleep(1)
+        print('1. Pinging tools: ')
+        time.sleep(1)
+        print('2. Browser history spammer: ')
+        time.sleep(1)
+        print('3. Computer destroyer: ')
+        time.sleep(1)
+        print('4. Discord spammer: ')
+        time.sleep(1)
+        print('5. Something idk')
+        time.sleep(1)
+        print('6. Other: ')
+        time.sleep(1)
+        print('7. Exit: ')
+        time.sleep(2)
+        a = int(input('Choose a Tool: '))
+        if a == 1:
+            print('1. Pinging tools selected!')
+            time.sleep(1)
+            print(
+                "....................................................................................................")
+            time.sleep(0.1)
+            for _ in range(15):
+                print(' ')
+
+
+
+        elif a == 2:
+            print('2. Browser history spammer selected!')
+            time.sleep(1)
+            print(
+                "....................................................................................................")
+            time.sleep(1)
+            for _ in range(15):
+                print(' ')
+            time.sleep(3)
+            print(
+                '▄▄▄▄    ██▀███   ▒█████   █     █░  ██████ ▓█████  ██▀███      ██░ ██  ██▓  ██████ ▄▄▄█████▓ ▒█████   ██▀███ ▓██   ██▓     ██████  ██▓███   ▄▄▄       ███▄ ▄███▓ ███▄ ▄███▓▓█████  ██▀███ ')
+            print(
+                '▓█████▄ ▓██ ▒ ██▒▒██▒  ██▒▓█░ █ ░█░▒██    ▒ ▓█   ▀ ▓██ ▒ ██▒   ▓██░ ██▒▓██▒▒██    ▒ ▓  ██▒ ▓▒▒██▒  ██▒▓██ ▒ ██▒▒██  ██▒   ▒██    ▒ ▓██░  ██▒▒████▄    ▓██▒▀█▀ ██▒▓██▒▀█▀ ██▒▓█   ▀ ▓██ ▒ ██▒')
+            print(
+                '▒██▒ ▄██▓██ ░▄█ ▒▒██░  ██▒▒█░ █ ░█ ░ ▓██▄   ▒███   ▓██ ░▄█ ▒   ▒██▀▀██░▒██▒░ ▓██▄   ▒ ▓██░ ▒░▒██░  ██▒▓██ ░▄█ ▒ ▒██ ██░   ░ ▓██▄   ▓██░ ██▓▒▒██  ▀█▄  ▓██    ▓██░▓██    ▓██░▒███   ▓██ ░▄█ ▒')
+            print(
+                '▒██░█▀  ▒██▀▀█▄  ▒██   ██░░█░ █ ░█   ▒   ██▒▒▓█  ▄ ▒██▀▀█▄     ░▓█ ░██ ░██░  ▒   ██▒░ ▓██▓ ░ ▒██   ██░▒██▀▀█▄   ░ ▐██▓░     ▒   ██▒▒██▄█▓▒ ▒░██▄▄▄▄██ ▒██    ▒██ ▒██    ▒██ ▒▓█  ▄ ▒██▀▀█▄  ')
+            print(
+                '░▓█  ▀█▓░██▓ ▒██▒░ ████▓▒░░░██▒██▓ ▒██████▒▒░▒████▒░██▓ ▒██▒   ░▓█▒░██▓░██░▒██████▒▒  ▒██▒ ░ ░ ████▓▒░░██▓ ▒██▒ ░ ██▒▓░   ▒██████▒▒▒██▒ ░  ░ ▓█   ▓██▒▒██▒   ░██▒▒██▒   ░██▒░▒████▒░██▓ ▒██▒')
+            print(
+                '░▒▓███▀▒░ ▒▓ ░▒▓░░ ▒░▒░▒░ ░ ▓░▒ ▒  ▒ ▒▓▒ ▒ ░░░ ▒░ ░░ ▒▓ ░▒▓░    ▒ ░░▒░▒░▓  ▒ ▒▓▒ ▒ ░  ▒ ░░   ░ ▒░▒░▒░ ░ ▒▓ ░▒▓░  ██▒▒▒    ▒ ▒▓▒ ▒ ░▒▓▒░ ░  ░ ▒▒   ▓▒█░░ ▒░   ░  ░░ ▒░   ░  ░░░ ▒░ ░░ ▒▓ ░▒▓░')
+            print(
+                '▒░▒   ░   ░▒ ░ ▒░  ░ ▒ ▒░   ▒ ░ ░  ░ ░▒  ░ ░ ░ ░  ░  ░▒ ░ ▒░    ▒ ░▒░ ░ ▒ ░░ ░▒  ░ ░    ░      ░ ▒ ▒░   ░▒ ░ ▒░▓██ ░▒░    ░ ░▒  ░ ░░▒ ░       ▒   ▒▒ ░░  ░      ░░  ░      ░ ░ ░  ░  ░▒ ░ ▒░')
+            print(
+                '░ ░ ░ ░ ▒    ░   ░  ░  ░  ░     ░     ░░   ░     ░  ░░ ░ ▒ ░░  ░  ░    ░      ░ ░ ░ ▒    ░░   ░ ▒ ▒ ░░     ░  ░  ░  ░░         ░   ▒   ░      ░   ░      ░      ░     ░░   ░ ')
+            print(
+                '  ░ ░      ░          ░     ░  ░   ░         ░  ░  ░ ░        ░               ░ ░     ░     ░ ░              ░                 ░  ░       ░          ░      ░  ░   ░     ')
+            print(
+                '     ░                                                                                                        ░ ░          ')
+            for _ in range(3):
+                time.sleep(0.2)
+                print(' ')
+            print(
+                '.............................................................................................................................................................................................')
+            print(' ')
+            print('1. Random Google Page')
+            print('2. Random Website')
+            print('3. Selected Website')
+            print('4. Exit Tool')
+            c = int(input('Choose Tool: '))
+            if c == 1:
+                for _ in range(4):
+                    print(' ')
+                    time.sleep(.5)
+                print('1. Random google page selected!')
+                pages = int(input('How Many Tabs: '))
+
+                for _ in range(pages):
+                    webbrowser.open(url=('https://www.google.com/' + google_sites), new=1)
+                    time.sleep(1)
+                print('Completed task!')
+                time.sleep(2)
+                print('Returning to menu!')
+                time.sleep(1)
+            elif c == 2:
+                for _ in range(4):
+                    print(' ')
+                    time.sleep(.5)
+                print('2. Random website selected!')
+                pages = int(input('How Many Tabs: '))
+                for _ in range(pages):
+                    webbrowser.open(url=random_sites, new=1)
+                    time.sleep(1)
+                print('Completed task!')
+                time.sleep(2)
+                print('Returning to menu!')
+                time.sleep(1)
+            elif c == 3:
+                for _ in range(4):
+                    print(' ')
+                    time.sleep(.5)
+                print('3. Selected website!')
+                pages = int(input('How Many Tabs: '))
+                website = input('Website, You dont need http/s: ')
+                for _ in range(pages):
+                    webbrowser.open(url='http://' + website, new=1)
+                    time.sleep(1)
+                print('Completed task!')
+                time.sleep(2)
+                print('Returning to menu!')
+                time.sleep(1)
+            elif c == 4:
+                for _ in range(4):
+                    print(' ')
+                    time.sleep(.5)
+                print('Exitting!!!')
+                time.sleep(1)
+            else:
+                print('Wrong, That option does not exist!')
+                time.sleep(1)
+                print('Returning to menu!')
+                time.sleep(1)
+
+        elif a == 3:
+            print('3. Computer Destoryer Selected')
+            time.sleep(1)
+            print(
+                "....................................................................................................")
+            time.sleep(0.1)
+            for _ in range(15):
+                print(' ')
+
+        elif a == 4:
+            print('4. Discord spammer selected')
+            time.sleep(1)
+            print(
+                "....................................................................................................")
+            time.sleep(0.1)
+            for _ in range(15):
+                print(' ')
+        elif a == 5:
+            print('1. Pinging tools selected!')  # gotto work on this
+            time.sleep(1)
+            print(
+                "....................................................................................................")
+            time.sleep(0.1)
+            for _ in range(15):
+                print(' ')
+        elif a == 6:
+            print('6. Other Selected')
+            time.sleep(1)
+            print(
+                "....................................................................................................")
+            time.sleep(0.1)
+            for _ in range(15):
+                print(' ')
+        elif a == 7:
+            print('7. Exit Selected')
+            time.sleep(1)
+            print(
+                "....................................................................................................")
+            time.sleep(0.1)
+            for _ in range(15):
+                print(' ')
+            time.sleep(3)
+            b = input('You sure? Y/N: ')
+            if (b.capitalize()) == 'Y':
+                print('Terminating in 10 seconds')
+                time.sleep(5)
+                print('Terminating in 5 seconds!')
+                time.sleep(1)
+                print('Terminating in 4 seconds!')
+                time.sleep(1)
+                print('Terminating in 3 seconds!')
+                time.sleep(1)
+                print('Terminating in 2 seconds!')
+                time.sleep(1)
+                print('Terminating in 1 seconds!')
+                time.sleep(1)
+                print('TERMINATED')
+                exit()
+
+
